03c9509b800bd5d76de6b4f63d1cf48b5d606e82
Morgh <morghulis@qq.com>
2020-08-25 08:09:36 +0800

    this is a commit test

--- app/src/main/java/com/example/news_android/MainActivity.java
+++ app/src/main/java/com/example/news_android/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.news_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
